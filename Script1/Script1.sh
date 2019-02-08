1be561b722c47f1cff0605f6c0e505f2e1998107
NiilShon <51centimos@gmail.com>
2019-02-09 00:10:53 +0100

    Update Script1.sh

---
+++ Script1/Script1.sh
@@ -0,0 +1,27 @@
+echo -e '#######################################'
+echo -e '#  Bienvenido al script rico rico     #'
+echo -e '#  ANACONDA,TENSORFLOW,KERAS,         #'
+echo -e '#                                     #'
+echo -e '#  Este script esta en construccion.  #'
+echo -e '#               DEEP                  #'
+echo -e '#             EXPLOIT                 #'
+echo -e '# Instalacion Automatica ArchLinux    #'
+echo -e '#          By: Niil78                 #'
+echo -e '# No garantizo la instalacion correcta#'
+echo -e '#######################################'
+#https://codeby.net/threads/deep-exploit-fully-automatic-pentest-mashinnoe-obuchenie.63759/
+sleep 5
+echo -e 'Descarga e instalacion de anaconda'
+sleep 1
+echo -e 'Anaconda es una distribución de Python y R junto con las principales bibliotecas de análisis de datos y el administrador de paquetes conda.'
+sleep 1
+echo -e 'Con este último, es conveniente instalar y eliminar paquetes. También una gran ventaja es la instalación sin compilar desde el código fuente, lo que lo acelera significativamente en comparación con el pip'
+sleep 1
+echo -e 'Conda- Python package manager, le permite instalar paquetes ya compilados (puede funcionar en el modo de compilación de paquetes antes de la instalación).'
+sleep 1
+echo -e 'Conda también es el administrador del entorno del sistema, le permite crear entornos con diferentes versiones de cualquier cosa (bibliotecas C, bibliotecas de bajo nivel, etc.).'
+sleep 10
+cd /opt/
+wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+chmod +x Anaconda3-5.2.0-Linux-x86_64.sh
+./ Anaconda3-5.2.0-Linux-x86_64.sh
