c0e78e23adb32956f679c36debc8a98c20bf09db
NiilShon <51centimos@gmail.com>
2019-02-09 01:53:26 +0000

    º

--- Script1/msf.sh
+++ Script1/msf.sh
@@ -1,32 +1,42 @@
-pacman –Syyu
-pacman -S postgresql
-curl -L https://get.rvm.io | bash -s stable
-##-Descargar Metasploit desde Github:
+sudo pacman –Syy
+sudo pacman -S postgresql
+sudo curl -L https://get.rvm.io | bash -s stable
+echo -e '-Descargar Metasploit desde Github'
+sleep 1
 cd /opt
-git clone https://github.com/rapid7/metasploit-framework.git
-cd Metasploit-framework
+sudo git clone https://github.com/rapid7/metasploit-framework.git
+sleep 2
+cd metasploit-framework
+sleep 3
 bundle install
+sleep 5
 cd
 ##-Descomente la línea con la que necesitamos - en_US.utf8.
-nano /etc/locale.gen
-locale-gen
-##-Configurando la base de datos:
-chown -R postgres:postgres /var/lib/postgres/
-su postgresinitdb –locale en_US.UTF-8 -D ‘/var/lib/postgres/data’exit
+sudo nano /etc/locale.gen
+sleep 1
+sudo locale-gen
+echo -e '-Configurando la base de datos'
+sleep 10
+sudo chown -R postgres:postgres /var/lib/postgres/
+su postgres -c "initdb –locale en_US.UTF-8 -D ‘/var/lib/postgres/data’"
 systemctl start postgresql
 systemctl enable postgresql
-#-Crear un usuario y una base de datos para Metasploit
+echo -e '-Crear un usuario y una base de datos para Metasploit'
+echo -e 'contraseña para postgres'
+sudo passwd postgres
+sleep 1
+echo -e 'cambiar a postgres'
 su postgres
-createuser msf -P -S -R -D
-createdb -O msf msf
+createuser n78 -P -S -R -D
+createdb -O n78 n78
 ###              ###
 ### DEPENDENCIAS ###
 ###              ###
-pacman -Syu wget git gcc patch curl zlib readline autoconf automake diffutils make dk7-openjdk
-pacman -S libtool bison subversion gnupg python python2-pysqlite-legacy gtk2 pygtk libpcap
+sudo pacman -Syu wget git gcc patch curl zlib readline autoconf automake diffutils make dk7-openjdk
+sudo pacman -S libtool bison subversion gnupg python python2-pysqlite-legacy gtk2 pygtk libpcap
 
 #
-# db_connect msf@msf
+# db_connect n78@78
 # db_rebuild_cache
 # db_status
 # load msgrpc ServerHost=192.168.0.104 ServerPort=55553 User=test Pass=test1234
