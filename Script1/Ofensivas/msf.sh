ac565c69e48be0d97cbefccc50e4d07b9e1c27bc
NiilShon <51centimos@gmail.com>
2019-02-09 20:25:18 +0100

    update

--- Script1/Ofensivas/msf.sh
+++ Script1/Ofensivas/msf.sh
@@ -11,29 +11,6 @@ sleep 3
 bundle install
 sleep 5
 cd
-##-Descomente la línea con la que necesitamos - en_US.utf8.
-sudo nano /etc/locale.gen
-sleep 1
-sudo locale-gen
-echo -e '-Configurando la base de datos'
-sleep 10
-sudo chown -R postgres:postgres /var/lib/postgres/
-su postgres -c "initdb –locale $LANG -E UTF-8 -D ‘/var/lib/postgres/data’";exit
-systemctl start postgresql
-systemctl enable postgresql
-echo -e '-Crear un usuario y una base de datos para Metasploit'
-echo -e 'contraseña para postgres'
-sudo passwd postgres
-sleep 1
-echo -e 'cambiar a postgres'
-su postgres
-slep 1
-echo -e 'creando usuario para la base de datos'
-createuser n78 -P -S -R -D
-createdb -O n78 n78
-cat > /opt/metasploit-framework/config/database.yml
-sleep 5
-
 ###              ###
 ### DEPENDENCIAS ###
 ###              ###
@@ -41,10 +18,10 @@ sudo pacman -Syu wget git gcc patch curl zlib readline autoconf automake diffuti
 sudo pacman -S libtool bison subversion gnupg python python2-pysqlite-legacy gtk2 pygtk libpcap
 
 #
-# db_connect n78@78
+# db_connect msf@msf
 # db_rebuild_cache
 # db_status
-# load msgrpc ServerHost=192.168.0.104 ServerPort=55553 User=test Pass=test1234
+# load msgrpc ServerHost=localhost ServerPort=55553 User=msf Pass=msf1234
 # nano config.ini
 # python DeepExploit.py -t 192.168.X.X -m train
 # python CreateReport.py
