75b652965b2e85e333bce5097ef2ca7f039c1a16
NiilShon <51centimos@gmail.com>
2019-02-10 02:06:24 +0100

    1

--- MsfMarron/Ofensivas/MetaSploit/install.sh
+++ MsfMarron/Ofensivas/MetaSploit/install.sh
@@ -2,9 +2,9 @@ echo -e '######################################################################'
 echo -e '######################################################################'
 echo -e '# Instalador automatico para arch linux |##############################'
 echo -e '# 1º Instala PostGreSql                 |########### BY ##############'
-echo -e '# 2º Instala MetaSploit desde GitHub    |######### NIIL78 #############'
-echo -e '# 3º Configura una base de datos        |##############################'
-echo -e '######################################################################'
+echo -e '# 2º Instala Ruby                       |######### NIIL78 #############'
+echo -e '# 3º Instala MetaSploit desde GitHub    |##############################'
+echo -e '# 4º Configura una base de datos##########################################'
 echo -e '######################################################################'
 sleep 5
 chmod +x postgresql.sh
@@ -12,6 +12,8 @@ chmod +x msf.sh
 chmod +x msf-1.sh
 
 sh postgresql.sh
+clear
+sh ruby.sh
 clear
 sh msf.sh
 clear
